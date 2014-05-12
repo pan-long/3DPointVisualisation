db2e81a07ac78dd8a8b5c0d126527d42054c06f9
Pan Long <aga.panlong@me.com>
2014-05-12 14:53:26 +0800

    added point class

---
+++ point.java
@@ -0,0 +1,25 @@
+public class point{
+    private double x, y, z;
+
+    public point(double x, double y, double z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getX()
+    {
+        return this.x;
+    }
+
+    public double getY()
+    {
+        return this.y;
+    }
+
+    public double getZ()
+    {
+        return this.z;
+    }
+}
