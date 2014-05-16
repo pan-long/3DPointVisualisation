38600b19dc7600a70f5e08b22b9c6259f2252a2d
Pan Long <aga.panlong@me.com>
2014-05-16 16:10:19 +0800

    added set center of mass functionality in scale configuration

--- ScaleConfiguration.java
+++ ScaleConfiguration.java
@@ -47,6 +47,13 @@ class ScaleConfiguration {
         return centerOfMass;
     }
 
+    public void setCenterOfMass(double x, double y, double z)
+    {
+        movedCenterOfMass[0] = x;
+        movedCenterOfMass[1] = y;
+        movedCenterOfMass[2] = z;
+    }
+
     public double[] calculateCenterOfMass()
     {
         double sumX, sumY, sumZ;
