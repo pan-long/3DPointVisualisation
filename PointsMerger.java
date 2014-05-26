cc3f9f1c3346cc996e5c9550957754f54c2bc963
Pan Long <aga.panlong@me.com>
2014-05-26 10:52:29 +0800

    implement PointsMerger

--- PointsMerger.java
+++ PointsMerger.java
@@ -1,6 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 class PointsMerger {
-    public PointsMerger(){
-    
+    private List<point> pointsList = null;
+    private List<point> mergedPointsList = null;
+    private ScaleConfiguration sc = null;
+    /* private double scaleFactor = 0; */
+    private double radius = 0;
+    private double minDis = 0;
+
+    public PointsMerger(List<point> points){
+        pointsList = points;
+        sc = new ScaleConfiguration(points, 10);
+        mergedPointsList = new ArrayList<point> ();
+        /* scaleFactor = sc.getScaleFactor(); */
+        radius = sc.getRadius();
+        minDis = 3 * radius;
+    }
+
+    public List<point> getMergedPoints(){
+        for (point p : pointsList){
+            if (mergedPointsList.isEmpty())
+                mergedPointsList.add(p);
+            else{
+                boolean isMerged = false;
+                for (point mergedPoint : mergedPointsList){
+                    if (p.disTo(mergedPoint) < minDis){
+                        isMerged = true;
+                        break;
+                    }
+                }
+
+                if (!isMerged)
+                    mergedPointsList.add(p);
+            }
+        }
+
+        return mergedPointsList;
     }
 }
